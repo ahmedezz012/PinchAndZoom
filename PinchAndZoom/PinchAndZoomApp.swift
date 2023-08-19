389c5698f342b07d128320bb623f38cab3483e0c
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-08-20 00:22:54 +0300

    Initial Commit

---
+++ PinchAndZoom/PinchAndZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAndZoomApp.swift
+//  PinchAndZoom
+//
+//  Created by Ahmed Ezz on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchAndZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
